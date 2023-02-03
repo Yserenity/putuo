83e7df59ffdb63ce97162c2e453a411bd984f3d2
胡崚晖 <2472497634@qq.com>
2023-02-03 09:21:15 +0800

    普元

--- src/test/java/com/dataojo/putuo/PutuoApplicationTest.java
+++ src/test/java/com/dataojo/putuo/PutuoApplicationTest.java
@@ -10,25 +10,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 @SpringBootTest
-@MapperScan("com.dataojo.putuo.mapper")
 public class PutuoApplicationTest {
-    @Resource
-    PutuoPipeiMapper putuoPipeiMapper;
     @Test
-    void a() {
-        PutuoPipei putuoPipei = null;
-        for (int i = 199; i < 1219; i++){
-            putuoPipei = putuoPipeiMapper.select(i);
-            String s = putuoPipei.getAddress().split(" ")[0];
-            String baseUrl = "http://158.10.0.222/wk/proxy/Search/api/Address/Search?keyWord=" + s + "&pageSize=1000&area=普陀区&pageIndex=1";
-            Map<String, String> queryMap = new HashMap();
-            JSONObject jsonObject = JSONObject.parseObject(InterTest.getResponseByMap(baseUrl,queryMap,"IPhj81.qyUqPr.wWJDx6"));
-            int result = Integer.parseInt(jsonObject.getString("matchCount"));
-            putuoPipeiMapper.update(i,result);
-        }
+    void test(){
     }
 }
