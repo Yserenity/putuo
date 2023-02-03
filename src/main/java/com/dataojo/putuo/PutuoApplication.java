bc5399f4fa3e77bd46748d10bab227159042189a
胡崚晖 <2472497634@qq.com>
2023-02-03 09:29:48 +0800

    普元：main方法

--- src/main/java/com/dataojo/putuo/PutuoApplication.java
+++ src/main/java/com/dataojo/putuo/PutuoApplication.java
@@ -2,13 +2,17 @@ package com.dataojo.putuo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class PutuoApplication {
-
+public class PutuoApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PutuoApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(PutuoApplication.class, args);
     }
-
 }
